2693244fa5525b698beabc1ba01ec76a330b0821
Rishita Panchal <rishita.p@simformsolutions.com>
2025-07-22 18:30:46 +0530

    Add Room paging library

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -156,6 +156,7 @@ dependencies {
     annotationProcessor(libs.androidx.room.compiler)
     ksp(libs.androidx.room.compiler)
     implementation(libs.androidx.room.ktx)
+    implementation(libs.androidx.room.paging)
     
     // Paging 3
     implementation(libs.androidx.paging.runtime)
