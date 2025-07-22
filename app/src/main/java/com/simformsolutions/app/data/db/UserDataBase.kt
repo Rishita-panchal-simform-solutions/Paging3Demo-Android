17936b38f556abd4efb4dcf7ffc118b4d5e0660c
Rishita Panchal <rishita.p@simformsolutions.com>
2025-07-22 19:28:39 +0530

    Add remote mediator

--- app/src/main/java/com/simformsolutions/app/data/db/UserDataBase.kt
+++ app/src/main/java/com/simformsolutions/app/data/db/UserDataBase.kt
@@ -2,18 +2,15 @@ package com.simformsolutions.app.data.db
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
-import androidx.room.TypeConverters
 import com.simformsolutions.app.data.db.dao.UserDao
 import com.simformsolutions.app.data.db.dao.UserRemoteKeysDao
 import com.simformsolutions.app.data.db.entity.UserRemoteKeys
 import com.simformsolutions.app.data.db.entity.UserResponseEntity
-import com.simformsolutions.app.data.db.typeconverter.Converters
 
 @Database(entities = [
     UserResponseEntity::class,
     UserRemoteKeys::class
 ], version = 1, exportSchema = false)
-@TypeConverters(Converters::class)
 abstract class UserDataBase : RoomDatabase() {
     abstract fun userDao(): UserDao
     abstract fun userRemoteKeysDao(): UserRemoteKeysDao
---
+++ app/src/main/java/com/simformsolutions/app/data/paging/UserRemoteMediator.kt
@@ -0,0 +1,26 @@
+package com.simformsolutions.app.data.paging
+
+import androidx.paging.ExperimentalPagingApi
+import androidx.paging.LoadType
+import androidx.paging.PagingState
+import androidx.paging.RemoteMediator
+import com.simformsolutions.app.data.db.UserDataBase
+import com.simformsolutions.app.domain.model.User
+import com.simformsolutions.app.domain.repository.UsersRepository
+import javax.inject.Inject
+
+@OptIn(ExperimentalPagingApi::class)
+class UserRemoteMediator @Inject constructor(
+    private val usersRepository: UsersRepository,
+    private val userDataBase: UserDataBase
+): RemoteMediator<Int, User>() {
+
+    private val userDao = userDataBase.userDao()
+    private val remoteKeysDao = userDataBase.userRemoteKeysDao()
+
+//    @ExperimentalPagingApi
+//    override suspend fun load(loadType: LoadType, state: PagingState<Int, User>): MediatorResult {
+//        retu
+//    }
+
+}
